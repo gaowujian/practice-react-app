fa02d366631f53dd65231c57d55c43fc619850a8
Tony Gao <806003097@qq.com>
2020-07-02 14:49:15 +0800

    react-use实践，用着还不错

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
-import useSWR from "swr";
+import { useAsync } from "react-use";
 import { getUser } from "./service";
-function App() {
-  const { data, error } = useSWR("/api/user", getUser);
-  console.log(data, error);
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+const App = () => {
+  const state = useAsync(getUser, []);
+  console.log(state);
   return (
     <div>
-      hello {data.user} {data.age}!
+      {state.loading ? (
+        <div>Loading...</div>
+      ) : state.error ? (
+        <div>Error: {state.error.message}</div>
+      ) : (
+        <div>
+          <h2>user: {state.value.user}</h2>
+          <h2>age:{state.value.age}</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
+
 export default App;
