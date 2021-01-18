131e1b29194d7c983f2a7fffa0743f1a9370a8ca
tony <gwjlshebei@gmail.com>
2021-01-18 16:16:37 +0800

    使用动画组件

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import "./style.css";
 
-const style = {
-  height: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
 function App() {
-  return <div style={style}>一个默认的本地空白项目用于练习各种不同的想法</div>;
+  const [show, setShow] = useState(false);
+  const contentRef = useRef();
+  return (
+    <div>
+      <section>
+        <h1>使用js操作ref</h1>
+        <div className="enter" ref={contentRef}>
+          我是一个内容区域
+        </div>
+        <br />
+        <button
+          onClick={() => {
+            contentRef.current &&
+              contentRef.current.classList.toggle("enter-active");
+          }}
+        >
+          显示/隐藏
+        </button>
+      </section>
+      <section>
+        <h1>使用react-transition-group</h1>
+
+        <CSSTransition in={show} appear timeout={2000} classNames="my-node">
+          <div className={!show ? "my-node-base" : ""}>我是一个内容区域</div>
+        </CSSTransition>
+
+        <br />
+        <button
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          显示/隐藏
+        </button>
+      </section>
+    </div>
+  );
 }
 
 export default App;
