214409365801b2350ef77b7784422da9b9415a9a
gwjlshebei <806003097@qq.com>
2022-07-25 22:45:54 +0800

    事件测试

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useMemo } from "react";
+import "./style.css";
 
-const style = {
-  height: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
 function App() {
-  return <div style={style}>一个默认的本地空白项目用于练习各种不同的想法</div>;
+  const mouseEventManager = useMemo(() => {
+    return {
+      onClick: function (e) {
+        console.log(`${e.currentTarget.id}触发了click`);
+      },
+      onMouseDown: function (e) {
+        console.log(`${e.currentTarget.id}触发了onMouseDown`);
+      },
+      onMouseUp: function (e) {
+        console.log(`${e.currentTarget.id}触发了onMouseUp`);
+      },
+    };
+  }, []);
+  return (
+    <div id="parent" {...mouseEventManager}>
+      父元素
+      <div id="child" {...mouseEventManager}>
+        子元素
+      </div>
+    </div>
+  );
 }
 
 export default App;
